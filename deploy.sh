52196876118262307dc66216eb455332d623b610
Hu Wenchao <zjuhwc@gmail.com>
2015-04-17 21:59:57 +0800

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-path='../my_wiki_book'
+path='/tmp/_book'
+rm -rf $path
 gitbook build -o $path
 cd $path
+git init
 git add -A
 git commit -m 'deploy'
-# git remote add origin git@github.com:yely/mywiki.git
-git push origin master:gh-pages
+git remote add origin git@github.com:yely/mywiki.git
+git push origin master:gh-pages --force
